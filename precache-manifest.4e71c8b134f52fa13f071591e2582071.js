ecc29b768f7ea1270f74d3d95f26a0d751e14c0a
wu kai <wukai2048@gmail.com>
2019-07-16 17:51:03 +0800

    Updates

---
+++ precache-manifest.4e71c8b134f52fa13f071591e2582071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32480b273bff8d9a8d39e55434616a55",
+    "url": "/draft-editor/index.html"
+  },
+  {
+    "revision": "596c38a6fe5ed592c7ab",
+    "url": "/draft-editor/static/css/2.a17ebd6e.chunk.css"
+  },
+  {
+    "revision": "816fd9dc9063e3b5c2d4",
+    "url": "/draft-editor/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "596c38a6fe5ed592c7ab",
+    "url": "/draft-editor/static/js/2.03b89e43.chunk.js"
+  },
+  {
+    "revision": "816fd9dc9063e3b5c2d4",
+    "url": "/draft-editor/static/js/main.22d56d94.chunk.js"
+  },
+  {
+    "revision": "c71ddb585e043080cb03",
+    "url": "/draft-editor/static/js/runtime~main.7721208c.js"
+  }
+]);
